7049f44e170988bb1439c3496f8eabeef264970b
MORI Kazutaka (XPS13) <mori.kazutaka@gmail.com>
2016-02-10 23:51:34 +0900

    XORShift : メソッドを整理

--- xorshift.js
+++ xorshift.js
@@ -1,7 +1,7 @@
-// Xorshift32による乱数発生器
+// XORShift32による乱数発生器
 //  JavaScriptのMath.random()は乱数のseedが設定できない
-//  同じ乱数表を再現できるよう、seedが設定できて実装が簡単なXorshiftで作成
-//  [参考] http://d.hatena.ne.jp/nakamura001/touch/20110521/1305997364
+//  同じ乱数表を再現できるよう、seedが設定できて実装が簡単なXORShiftで作成
+//  [参考] http://d.hatena.ne.jp/nakamura001/20110521/1305997364
 
 var XORShift = (function(){
     // seedsの格納用
@@ -9,20 +9,6 @@ var XORShift = (function(){
 
     // 公開メソッド
     return {
-        // debug用
-        show : function(){
-            console.log("seed x = ", this.x);
-            console.log("seed y = ", this.y);
-            console.log("seed z = ", this.z);
-            console.log("seed w = ", this.w);
-        },
-
-        // 乱数のシャッフル
-        shuffle : function(n){
-            // 回数を指定されていなければ10回分進める
-            for(var i = 0; i < (n ? n : 10); i++) this.rnd()
-        },
-
         // seedsの初期化
         setSeed : function(seed){
             this.x = 123456789;
@@ -30,6 +16,12 @@ var XORShift = (function(){
             this.z = 521288629;
             // seedが指定された場合は4つ目のseedをその値にする
             this.w = seed ? seed : 88675123;
+        },
+
+        // 乱数のシャッフル
+        shuffle : function(n){
+            // 回数を指定されていなければ10回分進める
+            for(var i = 0; i < (n ? n : 10); i++) this.rnd()
         },
 
         // 初期化（seed設定＋シャッフル）
@@ -41,10 +33,7 @@ var XORShift = (function(){
         // 乱数の取得
         rnd : function(){
             // 初期化がまだされていなければ初期化を行う
-            if(!this.x){
-                this.setSeed(Date.now());
-                this.shuffle(20);
-            }
+            if(!this.x) this.init(Date.now(), 20);
 
             // 32bit XORShift
             var t  = this.x ^ (this.x << 11);
