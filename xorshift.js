1067b543835db1595a308c871662f21f268438ce
MORI Kazutaka (XPS13) <mori.kazutaka@gmail.com>
2016-02-10 12:19:36 +0900

    クロージャに閉じ込める形に修正

--- xorshift.js
+++ xorshift.js
@@ -2,30 +2,51 @@
 //  JavaScriptのMath.random()は乱数のseedが設定できない
 //  同じ割付表を再現できるよう、seedが設定できて実装が簡単なXorshiftを自分で実装
 
-var XORShift = function(seed){
-    var x, y, z, w;     // seeds
+var XORShift = (function(){
+    // seedsの初期値
+    var x, y, z, w;
 
-    // seedsの初期化
-    this.setSeed = function(seed){
-        this.x = 123456789;
-        this.y = 362436069;
-        this.z = 521288629;
-        this.w = seed ? seed : 88675123;
+    // 公開メソッド
+    return {
+        // debug用
+        show : function(){
+            console.log("seed x = ", this.x);
+            console.log("seed y = ", this.y);
+            console.log("seed z = ", this.z);
+            console.log("seed w = ", this.w);
+        },
+
+        // 乱数のシャッフル
+        shuffle : function(n){
+            // 回数を指定されていなければ10回分進める
+            for(var i = 0; i < (n ? n : 10); i++) this.rnd()
+        },
+
+        // seedsの初期化
+        setSeed : function(seed){
+            this.x = 123456789;
+            this.y = 362436069;
+            this.z = 521288629;
+            this.w = seed ? seed : 88675123;
+        },
+
+        // 乱数の取得
+        rnd : function(){
+            // 初期化がまだされていなければ初期化を行う
+            if(!this.x){
+                this.setSeed(Date.now());
+                this.shuffle(20);
+            }
+
+            // 32bit  XORShift
+            var t  = this.x ^ (this.x << 11);
+            this.x = this.y;
+            this.y = this.z;
+            this.z = this.w;
+            this.w = (this.w ^ (this.w >>> 17)) ^ (t ^ (t >>> 13));
+
+            // 負の数にならないよう先頭ビットを0にして、0～1で返す
+            return (this.w & 0x7fffffff) / 0x7fffffff;
+        }
     }
-
-    // 乱数を取得
-    this.rnd = function(){
-        var t  = this.x ^ (this.x << 11);
-        this.x = this.y;
-        this.y = this.z;
-        this.z = this.w;
-        this.w = (this.w ^ (this.w >>> 19)) ^ (t ^ (t >>> 8));
-        // 負の数にならないよう先頭ビットを0にして、0～1で返す
-        return (this.w & 0x7fffffff) / 0x7fffffff;
-    }
-
-    // seedsの初期化
-    this.setSeed(seed);
-    // 10回分乱数を進めてseedsをシャッフル
-    for(var i = 0; i < 10; i++) this.rnd();
-}
+})()
