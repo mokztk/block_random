6ba12577d297ec4f699922542ab30ab5881af141
MORI Kazutaka (XPS13) <mori.kazutaka@gmail.com>
2016-02-22 16:18:21 +0900

    XORShiftのメソッドはprototypeを使って実装する形に変更

--- resource/xorshift.js
+++ resource/xorshift.js
@@ -8,82 +8,92 @@
 //  本文中に短いコードがあるのみでGPLが適用できるような長いコードではないことから、本文の
 //  Creative Commons Attribution 3.0 Unported License とみなされているらしい
 
+"use strict";
+
 /**
  *  128bit XORShift による擬似乱数発生器
  *  @constructor
  *  @param {number} [seed] - 4番目の乱数シードを変更する場合に指定
- *  @param {number} [n] - 乱数列を進める回数
+ *  @param {number} [n = 20] - 乱数列を進める回数
  */
 var XORShift = function (seed, n) {
-    /**
-	 *  seedsの格納用
-	 */
-    var x, y, z, w;
-				
+    var x, y, z, w, i;
+	
 	/**
 	 *  seedsの初期化
-	 *  @param {number} [seed] - 4番目のシードを変更する場合に指定
 	 */
-	this.setSeed = function (seed) {
-		this.x = 123456789;
-		this.y = 362436069;
-		this.z = 521288629;
-		/**
-		 *  seedが指定された場合は4つ目のseedをその値にする
-		 */
-		this.w = seed ? seed : 88675123;
-	};
+	this.x = 123456789;
+	this.y = 362436069;
+	this.z = 521288629;
+	/**
+	 *  seedが指定された場合は4つ目のseedをその値にする
+	 */
+	this.w = seed ? seed : 88675123;
 
 	/**
 	 *  乱数のシャッフル
-	 *  @param {number} [n = 20] - 乱数列を進める回数
+	 *    回数を指定されていなければ20回分進める
 	 */
-	this.shuffle = function (n) {
-		/**
-		 *  回数を指定されていなければ20回分進める
-		 */
-		var i;
-		for (i = 0; i < (n ? n : 20); i++) { this.rnd(); }
-	};
+	for (i = 0; i < (n ? n : 20); i++) { this.rnd(); }
+};
+
+/**
+ *  seedsの初期化
+ *  @param {number} [seed] - 4番目のシードを変更する場合に指定
+ */
+XORShift.prototype.setSeed = function (seed) {
+	this.x = 123456789;
+	this.y = 362436069;
+	this.z = 521288629;
+	/**
+	 *  seedが指定された場合は4つ目のseedをその値にする
+	 */
+	this.w = seed ? seed : 88675123;
+};
+
+/**
+ *  乱数のシャッフル
+ *  @param {number} [n = 20] - 乱数列を進める回数
+ */
+XORShift.prototype.shuffle = function (n) {
+	/**
+	 *  回数を指定されていなければ20回分進める
+	 */
+	var i;
+	for (i = 0; i < (n ? n : 20); i++) { this.rnd(); }
+};
+
+/**
+ *  乱数列を初期化（seed設定＋シャッフル）
+ *  @param {number} [seed] - 4番目のseedの初期値
+ *  @param {number} [n] - 乱数列を進める回数
+ */
+XORShift.prototype.init = function (seed, n) {
+	this.setSeed(seed);
+	this.shuffle(n);
+};
+
+/**
+ *  乱数をひとつ生成する（Math.random()互換）
+ *  @return {number} [0, 1) の範囲の数値
+ */
+XORShift.prototype.rnd = function () {
+	/**
+	 *  初期化がまだされていなければ初期化を行う
+	 */
+	if (!this.x) { this.init(Date.now(), 40); }
 
 	/**
-	 *  乱数列を初期化（seed設定＋シャッフル）
-	 *  @param {number} [seed] - 4番目のseedの初期値
-	 *  @param {number} [n] - 乱数列を進める回数
+	 *  128bit XORShift
 	 */
-	this.init = function (seed, n) {
-		this.setSeed(seed);
-		this.shuffle(n);
-	};
+	var t  = this.x ^ (this.x << 11);
+	this.x = this.y;
+	this.y = this.z;
+	this.z = this.w;
+	this.w = (this.w ^ (this.w >>> 17)) ^ (t ^ (t >>> 13));
 
 	/**
-	 *  乱数をひとつ生成する（Math.random()互換）
-	 *  @return {number} [0, 1) の範囲の数値
+	 *  >>>0 でunsignedに変換してから、[0, 1) の範囲にして返す
 	 */
-	this.rnd = function () {
-		/**
-		 *  初期化がまだされていなければ初期化を行う
-		 */
-		if (!this.x) { this.init(Date.now(), 40); }
-
-		/**
-		 *  128bit XORShift
-		 */
-		var t  = this.x ^ (this.x << 11);
-		this.x = this.y;
-		this.y = this.z;
-		this.z = this.w;
-		this.w = (this.w ^ (this.w >>> 17)) ^ (t ^ (t >>> 13));
-
-		/**
-		 *  >>>0 でunsignedに変換してから、[0, 1) の範囲にして返す
-		 */
-		return ((this.w >>> 0) % 0xffffffff) / 0xffffffff;
-	};
-	
-	/**
-	 *  初期化
-	 */
-	this.init(seed, n);
-	
+	return ((this.w >>> 0) % 0xffffffff) / 0xffffffff;
 };
